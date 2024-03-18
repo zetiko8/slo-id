97ca58bf30530914d760b8a333158ee3164307c0
anze.kolsek <anze.kolsek@gmail.com>
2024-03-18 20:56:27 +0100

    replaced invalid slovenian characters from email

--- library/src/email.ts
+++ library/src/email.ts
@@ -4,8 +4,8 @@ import { getRandomElementOfArray } from './helpers';
 export type EmailOptions = string | { baseEmail: string };
 
 export function generateEmail(
-  ime: string,
-  priimek: string,
+  firstName: string,
+  surname: string,
   options: EmailOptions = '',
 ): string {
   if (typeof options !== 'string' && options?.baseEmail === undefined)
@@ -13,14 +13,31 @@ export function generateEmail(
 
   if (typeof options === 'string' && options !== '') return options;
 
+  const nameForEmail = stringForEmail(firstName);
+  const surnameForEmail = stringForEmail(surname);
   if ((options as { baseEmail: string })?.baseEmail !== undefined) {
     const opts = options as { baseEmail: string };
     if (typeof opts.baseEmail !== 'string') throw ERROR.email.baseEmail;
     if (!opts.baseEmail.includes('@')) throw ERROR.email.baseEmail;
 
     const split = opts.baseEmail.split('@');
-    return [ split[0], '+', ime, '.', priimek, split[1] ].join('');
+    return [split[0], '+', nameForEmail, '.', surnameForEmail, split[1]].join(
+      '',
+    );
   }
   const emailSuffixes = ['@gmail.com', '@hotmail.com'];
-  return `${ime}.${priimek}${getRandomElementOfArray(emailSuffixes)}`;
+  return `${nameForEmail}.${surnameForEmail}${getRandomElementOfArray(
+    emailSuffixes,
+  )}`;
+}
+
+function stringForEmail(str: string) {
+  return str
+    .toLowerCase()
+    .replace(/č/g, 'c')
+    .replace(/š/g, 's')
+    .replace(/đ/g, 'd')
+    .replace(/ž/g, 'z')
+    .replace(/ć/g, 'c')
+    .replace(/\s/g, '.');
 }
